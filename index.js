e0296e875d605583db2e7ed6920c803286ec081d
helin <helin.tayeb1@gmail.com>
2024-04-18 15:50:23 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+function writeCards(messages){
+    let thankYouMessages = [];
+    
+    for (let i=0; i<messages.length; i++){
+        thankYouMessages.push(`Thank you, ${messages[i]}, for the wonderful surprise gift!`); 
+    }
+   return thankYouMessages;
+}
+
+
+function countDown(number){
+    for (let i=number; i>=0; i--){
+        console.log(i);
+    }
+}
+
+console.log(countDown(100))
